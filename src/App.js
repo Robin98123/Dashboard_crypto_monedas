0274be2eb21a39646f213a0e1531eb378831db15
Robin98123 <robinson1998sena@gmail.com>
2022-12-18 17:21:30 -0500

    se sube repositorio

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+/*   const getData = async () => {
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    const json = await response.json()
+    const response_cur = await fetch("https://api.coingecko.com/api/v3/simple/supported_vs_currencies")
+    const cur = await response_cur.json()
+  }
+  useEffect(() => {
+    getData()
+  } ) */
+
+
+  return 
+
+}
+
+export default App;
+
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import '../App.css'
+
+export function Header (){
+    return (
+    <header>
+        <h1>Crypto Stadistics</h1>
+        <nav>
+        <li>USD</li>
+        <li><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAJJJREFUSEvtk0EOgDAIBOGl6s/0pRiS1pAKXTTpDc/bnXYEpsUfL+6nAkDDpei/IhE5iUiY+Zi1oJz7D9qhrRWfESSTywCU84IM5Zq5mHkfXxtOkVPwQLLlCpuOqQdpZ7q+8Ob9JXAPHIi14GqxAQjQcACB5VCRvckASZV/ApiXkDct0a6kFMF1nQQKAO2VIqjoBicXQRmEVN84AAAAAElFTkSuQmCC"/></li>
+    </nav>
+    </header>
+    )
+}
