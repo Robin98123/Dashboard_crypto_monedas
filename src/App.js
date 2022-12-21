198f288b0853784afa155dd9b2024f01e034c32e
Robin98123 <robinson1998sena@gmail.com>
2022-12-21 10:37:18 -0500

    se suben ultimos comentarios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,80 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react' // se importan los Hooks
+import "./App.css"; // Importamos nuestra hoja de estilos
+import { Line } from "react-chartjs-2";
+// Importamos los componentes principales de la aplicación
+import axios from 'axios'; 
+import CardPrincipal from './CardPrincipal';
+import TableCoins from './TableCoins';
+import Card from './Card'
+import Convert from './Convert';
+import Footer from './Footer'
+import Header from './Header'
+import {ThemeProvider} from "./Context/ThemeProvider";
 
-function App() {
-/*   const getData = async () => {
-    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+//Exportamos los estados principales
+
+export default function App() {
+  //Coins: almacena el valor de todas las monedas que vienen de la api
+  const [coins, setCoins] = useState()
+  //Currency: almacena el valor de todas las divisas que vienen de la api
+  const [currency, setCurrency] = useState()
+  //SelCur: almacena el valor de divisa que esocge el usurio.
+  const [selCur, setSelCur] = useState("usd")
+
+  //consumimos la api por medio de async dentro de una variable para contenerla
+  const getData = async () =>{
+    //se alamacena la url de la api que se pidio de la pagina de gecko
+    const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${selCur}&order=market_cap_desc&per_page=4&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C90d%2C1y`)
+    //se contiene el arreglo de objeto dentro de una json
     const json = await response.json()
     const response_cur = await fetch("https://api.coingecko.com/api/v3/simple/supported_vs_currencies")
     const cur = await response_cur.json()
+    setCoins(json)
+    setCurrency(cur)
   }
   useEffect(() => {
     getData()
-  } ) */
+  },[])
+  useEffect(() => {
+    getData()
+  },[selCur])
 
-
-  return 
+  //retornamos el valor de cargando mientras toda la aplicacion se renderiza
+  return (
+    !coins ? "Cargando..." :(
+    <div className='App'>
+       <ThemeProvider>
+        <Header currencys={currency} fun={setSelCur} cur={selCur}/>
+       </ThemeProvider>
+      <main>
+        <CardPrincipal json={coins[0]} cur={selCur}/>
+        <div className="cards_con">
+          { coins.map(({id,symbol, image, current_price,price_change_percentage_30d_in_currency},index) =>{
+            if(index != 0) {
+             return <Card key={index} price={`${symbol} - ${current_price} ${selCur} `} porcentaje={deleteDec(price_change_percentage_30d_in_currency,2)} img={image} coinId={id} cur={selCur}/>
+            }
+          })
+          }
+        </div>
+      </main>
+      <Convert/>
+      <TableCoins coins={coins}/>
+      <Footer/>
+    </div>
+  )
+  )
 
 }
 
-export default App;
+//funcion usada para resetear los decimales
+export function deleteDec(val, decimal) {
+  return val.toFixed(decimal)
+}
 
+//asigna el color verde o rojo dependiendo del numero 
+export function colorDec(num){
+  return num > 0 ? "green" : "red"
+}
+
+//funcion para separar los miles
+export const numberF = Intl.NumberFormat("es-ES")
---
+++ src/Card.jsx
@@ -0,0 +1,21 @@
+
+//importamos los estilos de la card y el grafico que nos trae la api 
+
+import "./Card.css"
+import Graph from "./Graph"
+import {colorDec} from './App'
+
+export default function Card({coinId, cur, porcentaje, price, img}){
+    return (
+        <div className="card">
+            <img src={img} alt=""/>
+            <div className="con-main">
+                <div className="con-title">
+                    <h2 className={`price ${colorDec(porcentaje)}`}>{price}</h2>
+                    <h4 className={`porcentajes ${colorDec(porcentaje)}`}>{porcentaje}%</h4>
+                </div>
+                <Graph coin={coinId} currency={cur} color={colorDec(porcentaje)}/>
+            </div>
+        </div>
+    )
+}
